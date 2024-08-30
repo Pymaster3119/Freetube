f47fb86afe42b96933bb4a4506610628b4a790c1
Pymaster3119 <adi31tya@icloud.com>
2024-08-30 12:15:22 -0400

    Fixing speed

--- download.py
+++ download.py
@@ -1,14 +1,27 @@
-import moviepy.editor as moviepy
 import os
 import subprocess
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+def convert_to_mov(input_path, output_path):
+    command = [
+        'ffmpeg',
+        '-i', input_path,
+        '-c:v', 'libx264',
+        '-preset', 'fast',
+        '-c:a', 'aac',
+        '-strict', 'experimental',
+        '-threads', '8',
+        output_path
+    ]
+    subprocess.run(command, check=True)
+
+
 def download_youtube_video(url):
     try:
         os.system("pip3 install yt-dlp")
         os.system(f"yt-dlp -o 'output.%(ext)s' {url}")
-
-        video_clip = moviepy.VideoFileClip("output.mkv")
-        video_clip.write_videofile("output.mov", codec="libx264", audio_codec="aac")
-        video_clip.close()
+        os.system("rm -f output.mov")
+        convert_to_mov("output.mkv", "output.mov")
     except:
         pass
 
