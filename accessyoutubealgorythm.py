1b0606fef51b211c3b3d5b565067b970202e4cdc
Pymaster3119 <adi31tya@icloud.com>
2024-08-31 16:38:26 -0400

    Finishing the algorithm recommendations and adding it to my tkinter video

--- accessyoutubealgorythm.py
+++ accessyoutubealgorythm.py
@@ -4,8 +4,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import os
-import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 
 options = webdriver.ChromeOptions()
@@ -19,6 +19,7 @@ username = input("What is your username?")
 password = input("What is your password?")
 
 driver.get("https://www.youtube.com")
+actions = ActionChains(driver)
 
 #Click signin
 WebDriverWait(driver, 1000).until(lambda driver: driver.execute_script("return document.readyState") == "complete")
@@ -28,16 +29,38 @@ WebDriverWait(driver, 1000).until(lambda driver: driver.execute_script("return d
 
 #Send username
 WebDriverWait(driver, 1000).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[2]/div/div/div[1]/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input")))
-driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[2]/div/div/div[1]/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input").send_keys(username)
+actions.move_by_offset(776, 842).click().perform()
+actions.send_keys(username).perform()
+actions.move_by_offset(-776, -842).perform()
 WebDriverWait(driver, 1000).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[1]/div/div/button/div[3]")))
-driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[1]/div/div/button/div[3]").click()
+print(driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[1]/div/div/button/div[3]").location)
+actions.move_by_offset(737, 816).click().perform()
+actions.move_by_offset(-737, -816).perform()
 WebDriverWait(driver, 1000).until(lambda driver: driver.execute_script("return document.readyState") == "complete")
 
 #Send password
 WebDriverWait(driver, 1000).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[2]/div/div/div/form/span/section[2]/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input")))
-driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[2]/div/div/div/form/span/section[2]/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input").send_keys(password)
+actions.move_by_offset(776, 842).click().perform()
+actions.send_keys(password).perform()
+actions.move_by_offset(-776, -842).perform()
 WebDriverWait(driver, 1000).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[1]/div/div/button/div[3]")))
-driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[1]/div/div/button/div[3]").click()
+actions.move_by_offset(737, 816).click().perform()
+actions.move_by_offset(-737, -816).perform()
 WebDriverWait(driver, 1000).until(lambda driver: driver.execute_script("return document.readyState") == "complete")
+
+#Skip past security (if applicable)
+try:
+    WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div/div[2]/div/div/button/div[3]")))
+    driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div/div[2]/div/div/button/div[3]").click()
+    WebDriverWait(driver, 1000).until(lambda driver: driver.execute_script("return document.readyState") == "complete")
+except:
+    print('No security found')
+
+
+def find_recommendations():
+    pass
+
+if __name__ == "__main__":
+    find_recommendations()
 
 input()
