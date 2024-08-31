29ff27a6bc45773ddff61b0814ce7e9903c7d15e
Pymaster3119 <adi31tya@icloud.com>
2024-08-31 17:39:12 -0400

    FINALLY finishing the algorythm recomendations

--- accessyoutubealgorythm.py
+++ accessyoutubealgorythm.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+import time
 
 
 options = webdriver.ChromeOptions()
@@ -52,15 +53,36 @@ WebDriverWait(driver, 1000).until(lambda driver: driver.execute_script("return d
 try:
     WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div/div[2]/div/div/button/div[3]")))
     driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div/div[2]/div/div/button/div[3]").click()
-    WebDriverWait(driver, 1000).until(lambda driver: driver.execute_script("return document.readyState") == "complete")
 except:
     print('No security found')
 
+WebDriverWait(driver, 1000).until(lambda driver: driver.execute_script("return document.readyState") == "complete")
 
 def find_recommendations():
-    pass
+    links = []
+    xpath_num = 0
+    while len(links) != 10:
+        xpath_num += 1
+        xpath = f"/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-item-renderer[{xpath_num}]/div/ytd-rich-grid-media/div[1]/div[3]/div[2]/h3/a/yt-formatted-string"
+        element = driver.find_element(By.XPATH, xpath)
+        driver.execute_script("window.open(arguments[0], '_blank');", element.get_attribute('href'))
+        time.sleep(2)
+        original_tab = driver.current_window_handle
+        driver.switch_to.window(driver.window_handles[-1])
+        new_url = driver.current_url
+        print(f"The link leads to: {new_url}")
+        driver.close()
+        driver.switch_to.window(original_tab)
+        links.append(new_url)
+
+    return links
 
 if __name__ == "__main__":
     find_recommendations()
 
 input()
+
+# /html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-item-renderer[2]/div/ytd-rich-grid-media/div[1]/div[3]/div[2]/h3/a/yt-formatted-string
+# /html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-item-renderer[3]/div/ytd-rich-grid-media/div[1]/div[3]/div[2]/h3/a/yt-formatted-string
+# /html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-item-renderer[4]/div/ytd-rich-grid-media/div[1]/div[3]/div[2]/h3/a/yt-formatted-string
+# /html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-item-renderer[9]/div/ytd-rich-grid-media/div[1]/div[3]/div[2]/h3/a/yt-formatted-string
